4344455568c26fc6f6a78dac09521871448b21d1
Andrew Kemp <andrewkemp70@gmail.com>
2013-09-01 11:25:58 +1000

    mailer debug 7

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -9,19 +9,19 @@ class VisitorsController < ApplicationController
   end
 
   def create
-  	@visitor = Visitor.new(secure_params)
-
-    respond_to do |format|
-      if @visitor.save
-        Inquiry.inquiry_received(@visitor).deliver
-        Inquiry.inquiry_response(@visitor).deliver
-        format.html { redirect_to(@visitor, notice: 'Your inquiry has been sent') }
-        format.json { render json: @visitor, status: :created, location: @visitor }
-      else
-        format.html { render action: 'new' }
-        format.json { render json: @visitor.errors, status: :unprocessable_entity }
-      end
+    @visitor = Visitor.new(secure_params)
+    if @visitor.valid?
+      logger.info {"Starting email inquiry_received"}
+      Inquiry.inquiry_received(@visitor).deliver
+      logger.info {"Starting email inquiry_response"}
+      Inquiry.inquiry_response(@visitor).deliver
+      flash[:notice] = "Thanks #{@visitor.contactname}, your inquiry is being sent."
+      render :new
+    else
+      render :new
     end
+
+    
   end
 
 
